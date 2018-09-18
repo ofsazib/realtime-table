53f0b877eb11a0b44e0cb16cf9db491bd460ccc6
Omar Faruk Sazib <ofsazib@gmail.com>
2018-09-18 19:16:17 +0600

    Added form for adding new flight

--- app.py
+++ app.py
@@ -16,6 +16,23 @@ def index():
     return render_template('index.html', flights=flights)
 
 
+@app.route('/create', methods=["POST", "GET"])
+def create():
+    if request.method == "POST":
+        flight = request.form["flight"]
+        destination = request.form["destination"]
+        check_in = datetime.strptime(request.form["check_in"], '%d-%m-%Y %H:%M %p')
+        depature = datetime.strptime(request.form["depature"], '%d-%m-%Y %H:%M %p')
+        status = request.form["status"]
+        new_flight = Flight(flight, destination, check_in, depature, status)
+        db_session.add(new_flight)
+        db_session.commit()
+
+        return redirect("/create", code=302)
+    else:
+        flights = Flight.query.all()
+        return render_template('backend.html', flights=flights)
+
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
