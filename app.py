68dc1c0da985d9fbcb2c9ba1aac0add1f6ad46c4
Omar Faruk Sazib <ofsazib@gmail.com>
2018-10-01 12:20:10 +0600

    added update flight functionality and pusher configuration

--- app.py
+++ app.py
@@ -33,10 +33,42 @@ def create():
         flights = Flight.query.all()
         return render_template('backend.html', flights=flights)
 
+
+@app.route('/edit/<int:id>', methods=["POST", "GET"])
+def update_flight(id):
+    if request.method == "POST":
+        flight = request.form["flight"]
+        destination = request.form["destination"]
+        check_in = datetime.strptime(request.form["check_in"], '%d-%m-%Y %H:%M %p')
+        depature = datetime.strptime(request.form["depature"], '%d-%m-%Y %H:%M %p')
+        status = request.form["status"]
+        
+        edit_flight = Flight.query.get(id)
+        edit_flight.flight = flight
+        edit_flight.destination = destination
+        edit_flight.check_in = check_in
+        edit_flight.depature = depature
+        edit_flight.status = status
+        db_session.commit()
+
+        return redirect("/create", code=302)
+    else:
+        new_flight = Flight.query.get(id)
+        new_flight.check_in = new_flight.check_in.strftime("%d-%m-%Y %H:%M %p")
+        new_flight.depature = new_flight.depature.strftime("%d-%m-%Y %H:%M %p")
+        return render_template('update-flight.html', data=new_flight)
+
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
 
 # run Flask app
 if __name__ == "__main__":
+    pusher_client = pusher.Pusher(
+        app_id=os.getenv('PUSHER_APP_ID'),
+        key=os.getenv('PUSHER_KEY'),
+        secret=os.getenv('PUSHER_SECRET'),
+        cluster=os.getenv('PUSHER_CLUSTER'),
+        ssl=True
+    )
     app.run()
